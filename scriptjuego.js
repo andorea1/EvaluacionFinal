6bf1e0cdb87deb4d0eae10c7526cd16fea5ee0a6
Andrea Duran <andrea.duran@segurossura.com.ar>
2022-08-04 14:48:01 -0300

    mensaje

--- scriptjuego.js
+++ scriptjuego.js
@@ -1,32 +1,32 @@
-var imagenes=document.querySelectorAll('#caja-img img');
+var imagenes=document.querySelectorAll('#caja-img');
 zonaDestino1=document.getElementById('caja1');  
 zonaDestino2=document.getElementById('caja2'); 
 zonaDestino3=document.getElementById('caja3'); 
 
 function iniciar(){
     for(var i=0; i<imagenes.length; i++){
-    imagenes[i].addEventListener('dragstart', arrastrado, false);
-    imagenes[i].addEventListener('dragend', finalizado, false);
+    imagenes[i].addEventListener('dragstart', arrastrado);
+    imagenes[i].addEventListener('dragend', finalizado);
     }
 
     zonaDestino1.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
+    e.preventDefault(); });
     zonaDestino1.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    zonaDestino1.addEventListener('drop', soltado, false);
+    e.preventDefault(); });
+    zonaDestino1.addEventListener('drop', soltado1);
 
     zonaDestino2.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
+    e.preventDefault(); });
     zonaDestino2.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    zonaDestino2.addEventListener('drop', soltado2, false);
+    e.preventDefault(); });
+    zonaDestino2.addEventListener('drop', soltado2);
 
 
     zonaDestino3.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
+    e.preventDefault(); });
     zonaDestino3.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    zonaDestino3.addEventListener('drop', soltado3, false);
+    e.preventDefault(); });
+    zonaDestino3.addEventListener('drop', soltado3);
 
 }
 
@@ -41,36 +41,25 @@ function finalizado(e){
     elemento.style.display = "none";
 } 
 
-function soltado(e){
+function soltado1(e){
     e.preventDefault();
    var id=e.dataTransfer.getData('Text');
-    if(id!="imagen4"){
-    var src=document.getElementById(id).src;
-    zonaDestino1.innerHTML='<img src="'+src+'"height="400px" width="275px">';
-    }else{
-    zonaDestino1.innerHTML='la imagen no es admitida';
-    }
+   var src=document.getElementById(id).src;
+   zonaDestino1.innerHTML='<img src="'+src+'"height="400px" width="275px">';
 }
 
 function soltado2(e){
     e.preventDefault();
     var id=e.dataTransfer.getData('Text');
-    if(id!="imagen4"){
-        var src=document.getElementById(id).src;
+    var src=document.getElementById(id).src;
     zonaDestino2.innerHTML='<img src="'+src+'"height="400px" width="275px">';
-    }else{
-    zonaDestino2.innerHTML='la imagen no es admitida';
-    }
 }
+
 function soltado3(e){
     e.preventDefault();
     var id=e.dataTransfer.getData('Text');
-    if(id!="imagen4"){
-        var src=document.getElementById(id).src;
+    var src=document.getElementById(id).src;
     zonaDestino3.innerHTML='<img src="'+src+'"height="400px" width="275px">';
-    }else{
-    zonaDestino3.innerHTML='la imagen no es admitida';
-    }
 }
 
 function reiniciar() {
